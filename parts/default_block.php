615e5b96a77389e3e1052248cdd65f3ca14111d4
Victor Andeloci <victor.andeloci@gmail.com>
2022-10-16 21:43:05 -0300

    loader align & default post item size fix

--- parts/default_block.php
+++ parts/default_block.php
@@ -1,6 +1,6 @@
 <div class="default">
   <a alt="<?php echo the_title(); ?>" class="post" id="<?php the_id(); ?>" href="<?php echo get_permalink(); ?>">
-    <?php $thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'thumbnail'); if (empty($thumbnail)) $thumbnail = get_template_directory_uri() . '/img/default-image.png'; ?>
+    <?php $thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'medium'); if (empty($thumbnail)) $thumbnail = get_template_directory_uri() . '/img/default-image.png'; ?>
     <div class="post-thumb" style="background-image: url('<?= $thumbnail ?>');"></div>
     <div class="post-details">
       <span><?php echo get_the_category(get_the_id())[0]->name; ?></span>
