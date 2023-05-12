b27009533c243ca7453c3c2868cfa09ffc48e3f1
Victor Andeloci <victor.andeloci@gmail.com>
2023-05-12 03:57:19 -0300

    home finish

---
+++ parts/social.php
@@ -0,0 +1,7 @@
+<section id="social">
+  <div class="container">
+    <h2 class="subtitle">Segue lá</h2>
+    <?php get_template_part('elements/social_links'); ?>
+    <?php echo do_shortcode( '[youtube-feed]' ); ?>
+  </div>
+</section>
