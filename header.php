6a2879da98a54287ed0debd527504c5b14393455
Victor Andeloci <victor.andeloci@gmail.com>
2023-05-29 04:57:15 -0300

    small fixes

--- header.php
+++ header.php
@@ -8,15 +8,15 @@
 
     <?php wp_head(); ?>
 
-    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/plugins.min.css?v=2.0.1">
-    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style.css?v=2.0.1">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/plugins.min.css?v=2.0.2">
+    <link rel="stylesheet" href="<?= get_template_directory_uri() ?>/style.css?v=2.0.2">
 
-    <script src="<?= get_template_directory_uri() ?>/plugins.min.js?v=2.0.1" charset="utf-8" defer></script>
+    <script src="<?= get_template_directory_uri() ?>/plugins.min.js?v=2.0.2" charset="utf-8" defer></script>
     <script type="text/javascript">
       const siteUrl = '<?= get_site_url() ?>';
       const apiUrl = '<?= get_site_url() ?>/wp-admin/admin-ajax.php';
     </script>
-    <script src="<?= get_template_directory_uri() ?>/main.min.js?v=2.0.1" charset="utf-8" defer></script>
+    <script src="<?= get_template_directory_uri() ?>/main.min.js?v=2.0.2" charset="utf-8" defer></script>
   </head>
   <body>
     <header class="<?= (is_single() || is_page() || is_404()) ? 'back-filled' : '' ?>">
--- parts/comments.php
+++ parts/comments.php
@@ -1,36 +1,4 @@
 <div id="commentsContainer">
-    <h2 class="subtitle dark">Comentários</h2>
-    <div class="wp-comments-container">
-        <?php
-            $args = [
-                'post_id' => get_the_ID(),
-                'status' => 'approve',
-                'orderby'=>'comment_date',
-                'order'=>'ASC'
-            ];
-
-            $comments_query = new WP_Comment_Query;
-            $comments = $comments_query->query( $args );
-
-            if ($comments) :
-                foreach ($comments as $comment) :
-        ?>
-            <div class="comment-item">
-                <div class="author">
-                    <img src="<?= get_avatar_url($comment->comment_author_email) ?>" alt="">
-                    <div class="name-date">
-                        <span class="name"><?= $comment->comment_author ?></span>
-                        <span> - </span>
-                        <span class="date"><?= date('d/m/Y', strtotime($comment->comment_date)) ?></span>
-                    </div>
-                </div>
-                <p><?= $comment->comment_content ?></p>
-            </div>
-        <?php
-                endforeach;
-            endif;
-        ?>
-    </div>
     <div class="disqus-comments-container">
         <div id="disqus_thread"></div>
         <script>
