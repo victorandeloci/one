5407358b9ca2299fbab5ccdd076a472171653eb1
Victor Andeloci <victor.andeloci@gmail.com>
2023-06-01 18:27:32 -0300

    Update podcast_player.php

--- elements/podcast_player.php
+++ elements/podcast_player.php
@@ -4,7 +4,7 @@
               : trim(get_post_meta(get_the_ID(), 'episode_mp3_url', true)); // old migration url or feed monitor plugin url
   if (!empty($mp3Url)) :
 ?>
-  <audio controls preload="none">
+  <audio controls preload="metadata">
     <source src="<?= $mp3Url ?>">
   </audio>
 <?php endif; ?>
