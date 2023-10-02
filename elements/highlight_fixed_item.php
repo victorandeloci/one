8a20f002a8e29c0e29b8b0d9bef4dd22aa906170
Victor Andeloci <victor.andeloci@gmail.com>
2023-10-02 00:17:54 -0300

    fixed highlight & fixes

---
+++ elements/highlight_fixed_item.php
@@ -0,0 +1,22 @@
+<?php  $thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'large'); ?>
+<a 
+    href="<?= trim(get_post_meta(get_the_ID(), 'one_fixed_highlight_url', true)) ?>"
+    class="highlight-item"
+    draggable="false"
+    target="_blank"
+    rel="noopener nofollow"
+    style="background-image: url(<?= !empty($thumbnail) 
+                                        ? $thumbnail
+                                        : (get_template_directory_uri() . '/assets/img/default-image.png') ?>);">
+    <?php if (!empty(trim(get_post_meta(get_the_ID(), 'one_fixed_highlight_show_info', true))) && (bool) trim(get_post_meta(get_the_ID(), 'one_fixed_highlight_show_info', true)) == true) : ?>
+        <div class="overlay"></div>
+        <div class="content">
+            <h3><?= get_the_title() ?></h3>
+            <hr>
+            <p><?= get_the_excerpt() ?></p>
+        </div>
+    <?php else : ?>
+        <div class="overlay fixed"></div>
+        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M32 32C32 14.3 46.3 0 64 0H320c17.7 0 32 14.3 32 32s-14.3 32-32 32H290.5l11.4 148.2c36.7 19.9 65.7 53.2 79.5 94.7l1 3c3.3 9.8 1.6 20.5-4.4 28.8s-15.7 13.3-26 13.3H32c-10.3 0-19.9-4.9-26-13.3s-7.7-19.1-4.4-28.8l1-3c13.8-41.5 42.8-74.8 79.5-94.7L93.5 64H64C46.3 64 32 49.7 32 32zM160 384h64v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V384z"/></svg>
+    <?php endif; ?>
+</a>
--- parts/highlights.php
+++ parts/highlights.php
@@ -1,37 +1,53 @@
-<?php
-    $args = [
-        'post_type' => 'post',
-        'category_name' => 'highlight',
-        'order' => 'DESC',
-        'posts_per_page' => 8
-    ];
-
-    $query = new WP_Query($args);
-
-    if ($query->have_posts()) :
-?>
-
-    <section id="highlights">
-        <div class="container title">
-            <h2 class="subtitle">Destaques</h2>
-            <div class="highlight-controllers">
-                <button id="hlLeftBtn">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>  
-                </button>
-                <button id="hlRightBtn">
-                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
-                </button>
-            </div>
+<section id="highlights">
+    <div class="container title">
+        <h2 class="subtitle">Destaques</h2>
+        <div class="highlight-controllers">
+            <button id="hlLeftBtn">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>  
+            </button>
+            <button id="hlRightBtn">
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+            </button>
         </div>
-        <div class="highlight-container">
-            <div class="items">
-                <?php
+    </div>
+    <div class="highlight-container">
+        <div class="items">
+            <?php
+                // fixed highlights
+                wp_reset_query();
+
+                $args = [
+                    'post_type' => 'fixed_highlight',
+                    'order' => 'DESC',
+                    'posts_per_page' => -1
+                ];            
+                $query = new WP_Query($args);
+            
+                if ($query->have_posts()) {
+                    while ($query->have_posts()) {
+                        $query->the_post();
+                        get_template_part('elements/highlight_fixed_item');
+                    }
+                }
+
+                // highlight items
+                wp_reset_query();
+
+                $args = [
+                    'post_type' => 'post',
+                    'category_name' => 'highlight',
+                    'order' => 'DESC',
+                    'posts_per_page' => 8
+                ];            
+                $query = new WP_Query($args);
+            
+                if ($query->have_posts()) {
                     while ($query->have_posts()) {
                         $query->the_post();
                         get_template_part('elements/highlight_item');
                     }
-                ?>
-            </div>
+                }
+            ?>
         </div>
-    </section>
-<?php endif; ?>
+    </div>
+</section>
